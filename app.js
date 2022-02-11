07779cc98ff746a73a316ff2ed3e0eb41c054403
Hanibee <cyborgofthefuture@gmail.com>
2022-02-11 09:28:28 -0500

    pseudocode

---
+++ app.js
@@ -0,0 +1,18 @@
+//make a board with specific squares represented where the snake can travel to
+
+//add a light-up snake
+//enable snake mobility
+//make a continuous loop for the snake to move around the board
+
+//enable arrow keys to move the snake - right, left, up, and down
+
+//add a reset button
+
+//add a game-over message
+
+//add a points icon
+//set points equal to 0 at start of game
+//after light-up boxes are selected by snake, add points
+//make light-up boxes appear randomly across the board
+
+
