6a533aeaa9bf9b84895426c55e549cb05f2f5de6
Hanibee <cyborgofthefuture@gmail.com>
2022-02-11 11:26:13 -0500

    structure input

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-//make a board with specific squares represented where the snake can travel to
+//make a board with specific squares that represent where the snake can travel to
 
 //add a light-up snake
 //enable snake mobility
 //make a continuous loop for the snake to move around the board
 
 //enable arrow keys to move the snake - right, left, up, and down
+//make the snake move a certain number of pixels - 10 for direction
 
 //add a reset button
 
@@ -14,5 +15,7 @@
 //set points equal to 0 at start of game
 //after light-up boxes are selected by snake, add points
 //make light-up boxes appear randomly across the board
+
+
 
 
