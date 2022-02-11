8579372d848449b011f242dde3431f44fc66f7f6
Hanibee <cyborgofthefuture@gmail.com>
2022-02-11 13:20:23 -0500

    pseudocode added

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 //make a board with specific squares that represent where the snake can travel to
+    //sorted by pixels
 
 //add a light-up snake
+    //determine length of snake
+
 //enable snake mobility
-//make a continuous loop for the snake to move around the board
+    //make a continuous loop for the snake to move around the board
 
 //enable arrow keys to move the snake - right, left, up, and down
 //make the snake move a certain number of pixels - 10 for direction
+function movebyArrows (event) {
+const RIGHT_KEY: 37;
 
-//add a reset button
+const left-key: 39;
+
+const up-key: 38;
+
+const down-key: 40;
+
+
+}
+
+
 
 //add a game-over message
+    //once snake touches border(specific pixels on the board), add game-over message
+//add a reset button
+    //event listener here that resets game when clicked
 
 //add a points icon
 //set points equal to 0 at start of game
