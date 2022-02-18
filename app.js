ec32018ad83ba9dd595b535827841e6a9826a9eb
Hanibee <cyborgofthefuture@gmail.com>
2022-02-17 20:43:11 -0500

    styling and working arrow keys

--- app.js
+++ app.js
@@ -25,9 +25,11 @@
 
 
 //variables
-let isWinner, lengthAdded
+let isWinner, lengthAdded, changedirection
 
 var score = 0;
+let speed = 7;
+let timing = 1000/speed;
 
 
 //cached element references
@@ -38,7 +40,10 @@ const cells = document.querySelectorAll('.cell');
 for (i=0; i < cells.length; i++) {
     idName = i.toString();
     cells[i].setAttribute('id', idName);
-    console.log(cells[i]);
+    if((i+1)%15==0){cells[i].classList.add("rightBorder"); console.log(cells[i]);}
+    if((i)%15==0){cells[i].classList.add("leftBorder"); console.log(cells[i]);}
+    if(i<15){cells[i].classList.add("topBorder"); console.log(cells[i]);}
+    if(i>=(15*15-15)){cells[i].classList.add("bottomBorder"); console.log(cells[i]);}
 }
 }
 
@@ -47,33 +52,128 @@ for (i=0; i < cells.length; i++) {
 
 document.addEventListener('keyup', event => {
 if (event.code === 'Space') {
-    console.log('space bar pressed')
-}
+    document.getElementById('pls_hide').style.visibility = "hidden";
+    spawnNewFood()
+    snake = document.querySelectorAll(".snake");
+    for (s=0; s<snake.length; s++){
+        snake[s].classList.remove("snake");}
+    document.getElementById('23').classList.add("snake");
+    console.log('space bar pressed');
+    changedirection = 0;
+    moveRight();
+    }
 });
 
 document.addEventListener('keyup', event => {
     if (event.code === 'ArrowDown') {
-        updateYPosition('.cell')
+        changedirection = 3;
+        moveDown();
     } if (event.code === 'ArrowUp') {
-        updateYPosition('.cell')
+        changedirection = 2;
+        moveUp();
     } if (event.code === 'ArrowLeft') {
-        updateXPosition(-moveRate)
+        changedirection = 1;
+        moveLeft();
     } if (event.code === 'ArrowRight') {
-        updateXPosition(-moveRate)
+        changedirection = 0;
+        moveRight();
     }
 });
 
 
+function moveRight(){
+    if (gameover()){console.log("gameover"); return;}
+    if (changedirection!=0){return}
+    setTimeout(function onTick(){moveRight();}, timing)
+    const snake = document.querySelectorAll(".snake");
+    for (s=0;s<snake.length;s++){
+        let current_segment_id = snake[s].getAttribute('id');
+        let next_segment_id = (parseInt(current_segment_id) + 1).toString();
+        next_segment = document.getElementById(next_segment_id);
+        next_segment.classList.add("snake");
+        if (snakeyEat() != true){snake[s].classList.remove("snake");}
+    }
+}
+
+function moveLeft(){
+    if (gameover()){console.log("gameover"); return;}
+    if (changedirection!=1){return}
+    setTimeout(function onTick(){moveLeft();}, timing)
+    const snake = document.querySelectorAll(".snake");
+    for (s=0;s<snake.length;s++){
+        let current_segment_id = snake[s].getAttribute('id');
+        let next_segment_id = (parseInt(current_segment_id)-1).toString();
+        next_segment = document.getElementById(next_segment_id);
+        next_segment.classList.add("snake");
+        if (snakeyEat()!= true){snake[s].classList.remove("snake");}
+    }
+}
 
 
+function moveDown(){
+    if (gameover()){console.log("gameover"); return;}
+    if (changedirection!=3){return}
+    setTimeout(function onTick(){moveDown();}, timing)
+    const snake = document.querySelectorAll(".snake");
+    for (s=0;s<snake.length;s++){
+        let current_segment_id = snake[s].getAttribute('id');
+        let next_segment_id = (parseInt(current_segment_id) + 15).toString();
+        next_segment = document.getElementById(next_segment_id);
+        next_segment.classList.add("snake");
+        if (snakeyEat()!= true){snake[s].classList.remove("snake");}
+    }
+}
 
-//setinterval and settimeout
+function moveUp(){
+    if (gameover()){console.log("gameover"); return;}
+    if (changedirection!=2){return}
+    setTimeout(function onTick(){moveUp();}, timing)
+    const snake = document.querySelectorAll(".snake");
+    for (s=0;s<snake.length;s++){
+        let current_segment_id = snake[s].getAttribute('id');
+        let next_segment_id = (parseInt(current_segment_id) - 15).toString();
+        next_segment = document.getElementById(next_segment_id);
+        next_segment.classList.add("snake");
+        if (snakeyEat()!= true){snake[s].classList.remove("snake");}
+    }
+}
 
-//functions
-// form.addEventListener("reset", init)
+function snakeyEat(){
+    current_food = document.querySelectorAll(".food")[0]
+    if (current_food.classList.contains("snake")){
+        current_food.classList.remove("food");
+        spawnNewFood();
+        return true;        
+    }else{return false};
+}
 
-// for (var i = 0, len = ____.length; i < len; i++) {
-// }
+function spawnNewFood(){
+    if(document.querySelectorAll(".food")[0] != null){
+        document.querySelectorAll(".food")[0].classList.remove("food");}
+    foodlocation = document.getElementById((Math.floor(Math.random() * (224 - 0 + 1)) + 0).toString());
+    if(foodlocation.classList.contains("rightBorder") || foodlocation.classList.contains("leftBorder") || foodlocation.classList.contains("leftBorder") || foodlocation.classList.contains("topBorder") || foodlocation.classList.contains("bottomBorder")){
+        spawnNewFood();
+    }else{
+        foodlocation.classList.add("food");
+    }
+}
+
+function gameover(){
+    right_border = document.querySelectorAll(".rightBorder");
+    left_border = document.querySelectorAll(".leftBorder");
+    top_border = document.querySelectorAll(".topBorder");
+    bottom_border = document.querySelectorAll(".bottomBorder");
+    for (element=0; element<right_border.length; element++){
+        if(right_border[element].classList.contains("snake")){return true}};
+    for (element=0; element<left_border.length; element++){
+        if(left_border[element].classList.contains("snake")){return true}};
+    for (element=0; element<top_border.length; element++){
+        if(top_border[element].classList.contains("snake")){return true}};
+    for (element=0; element<bottom_border.length; element++){
+        if(bottom_border[element].classList.contains("snake")){return true}};
+    return false;
+    
+}
 
 
 function init() {
@@ -84,8 +184,6 @@ function init() {
         resetBtn.removeAttribute("hidden");
     }
 }
-
-
 
 
 
